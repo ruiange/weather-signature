6711a6d544079a609f8fec3a064cec66c8eabfd4
ruiange <ruiange@aliyun.com>
2023-12-29 11:53:36 +0800

    增加os字段和browser字段默认值

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.use((req, res, next) => {
     const clientIP = req.headers['x-forwarded-for'] || req.ip || 'Unknown';
-    // 使用模板字符串来提高可读性
     req.clientIP = `${clientIP} (Detected by middleware)`;
     next();
 });
@@ -16,16 +15,18 @@ app.use((req, res, next) => {
 app.use('/images', express.static('output'));
 
 app.get('/', async (req, res) => {
-    const clientIP = req.clientIP || 'Unknown';
-    const {query, protocol} = req;
-    const ip = clientIP.replace(/[^0-9.]/g, '');
-    const userAgent = req.get('User-Agent') || '';
-    const parser = new UAParser(userAgent);
+    const clientIP = req.clientIP || 'Unknown';  // 获取请求的客户端IP地址，如果获取不到则默认为'Unknown'
+    const {query, protocol} = req;  // 解构赋值，从req对象中获取query和protocol属性
+    const ip = clientIP.replace(/[^0-9.]/g, '');  // 将clientIP中的非数字和非点号字符替换为空字符串，并将结果赋值给ip变量
+    const userAgent = req.get('User-Agent') || '';  // 从req对象中获取'User-Agent'头信息，如果获取不到则默认为空字符串
+    const parser = new UAParser(userAgent);  // 创建一个UAParser对象，传入userAgent作为参数，并将结果赋值给parser变量
+    const os = `${parser.getOS().name ? parser.getOS().name+" "+parser.getOS().version:'Servers'}`
+    const browser = `${parser.getBrowser().name ? parser.getOS().name+" "+parser.getOS().version:'WeChat'}`
     const queryParameters = {
         ip,
         city: query.city || '',
-        os: `${parser.getOS().name} ${parser.getOS().version}` || 'unknown',
-        browser: `${parser.getBrowser().name}[${parser.getBrowser().version}]` || 'unknown',
+        os,
+        browser,
     };
     const generatedData = await getWeatherData(queryParameters);
     let imageName = ''
@@ -39,8 +40,8 @@ app.get('/', async (req, res) => {
         } else {
             res.sendFile(imagePath);
         }
-    }else{
-        res.json({msg:'生成失败'})
+    } else {
+        res.json({msg: '生成失败'})
     }
 });
 
