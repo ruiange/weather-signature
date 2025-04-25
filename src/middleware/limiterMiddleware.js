06b814ea6669cca59d7c384398dd1a0808984b1a
ruiange <bombingpig@outlook.com>
2025-04-25 15:50:22 +0800

    refactor(middleware): 调整接口限流配置
    -将最大请求次数从 10 次调整为 5次
    - 优化了注释，将"最多 100 次请求"修改为"最多 5 次请求"

--- src/middleware/limiterMiddleware.js
+++ src/middleware/limiterMiddleware.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 分钟
-    max: 10, // 最多 100 次请求
+    max: 5, // 最多 5次请求
     message: '请求太频繁，请稍后再试',
     standardHeaders: true, // 返回标准的 RateLimit-* 头部
     legacyHeaders: false,  // 不使用旧的 X-RateLimit-* 头部
